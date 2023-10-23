a899da08efe820509f0d4f7d0f60d8357a6eadd5
ShwetaBharambe5 <shwetabharambe21@gmail.com>
2023-10-23 23:17:43 +0530

    final project

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", async function() {
+    const apiUrl = `https://crudcrud.com/api/52e1cc9baa2f40d8904e8c12d9341cc6/products`;
 
-const apiUrl = `https://crudcrud.com/api/d9a8b99681e9456584a60025d67adefa/products`;
-
-function getProducts(){
-    axios.get(apiUrl)
-        .then(response => {
+    async function getProducts() {
+        try {
+            const response = await axios.get(apiUrl);
             const productList = document.getElementById('productList');
             productList.innerHTML = '';
 
@@ -13,7 +12,7 @@ function getProducts(){
 
                 listItem.innerHTML = `
                     $${product.sellingPrice} - ${product.productName} - ${product.category}
-                    <button data-product-id = "${product._id}" class = "delete-button">Delete</button>
+                    <button data-product-id="${product._id}" class="delete-button">Delete</button>
                 `;
 
                 productList.appendChild(listItem);
@@ -21,59 +20,44 @@ function getProducts(){
 
             const deleteButtons = document.querySelectorAll('.delete-button');
             deleteButtons.forEach(button => {
-                button.addEventListener('click', function(){
+                button.addEventListener('click', async function() {
                     const productId = this.getAttribute('data-product-id');
-                    deleteProduct(productId);
-                })
-            })
-        })
-        .catch(error => {
+                    await deleteProduct(productId);
+                });
+            });
+        } catch (error) {
             console.error('Error fetching products:', error);
-        });  
-}
+        }
+    }
 
-function addProduct(){
-    
-    const sellingPrice = document.getElementById('sellingPrice').value;
-    const productName = document.getElementById('productName').value;
-    const category = document.getElementById('category').value;
+    async function addProduct() {
+        const sellingPrice = document.getElementById('sellingPrice').value;
+        const productName = document.getElementById('productName').value;
+        const category = document.getElementById('category').value;
 
-    axios.post(apiUrl, { 
-        sellingPrice: sellingPrice, 
-        productName: productName, 
-        category: category 
-    })
-        .then(response => {
-            //if(response.status === 200){
-                console.log('Product added Successfully');
-            //}
-            //else{
-            //    console.error('Error adding product.');
-           // }
-        })
-        .catch(error => {
+        try {
+            await axios.post(apiUrl, {
+                sellingPrice: sellingPrice,
+                productName: productName,
+                category: category
+            });
+            console.log('Product added Successfully');
+        } catch (error) {
             console.error('Error adding product:', error);
-        });
-}
+        }
+    }
 
-function deleteProduct(productId){
-    axios.delete(apiUrl + '/' + productId)
-        .then(response => {
-            //if(response.status === 204){
-                console.log('Product deleted successfully.');
-                getProducts();
-            //}
-            //else{
-            //    console.error('Error deleting product.');
-            //}
-        })
-        .catch(error => {
+    async function deleteProduct(productId) {
+        try {
+            await axios.delete(apiUrl + '/' + productId);
+            console.log('Product deleted successfully.');
+            await getProducts();
+        } catch (error) {
             console.error('Error deleting product:', error);
-        });
-}
+        }
+    }
 
-document.getElementById('addProductButton').addEventListener('click', addProduct);
+    document.getElementById('addProductButton').addEventListener('click', addProduct);
 
-getProducts();
-
+    await getProducts();
 });
